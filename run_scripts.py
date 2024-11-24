e4b089a7c9283af5639dd7f4a7f87737688dd0b8
tjhv10 <achiyabennatan@gmail.com>
2024-11-25 00:03:56 +0200

    final

--- run_scripts.py
+++ run_scripts.py
@@ -32,10 +32,12 @@ def like_comment_follow(device, max_duration=3600 * 2):  # 1 hour = 3600 seconds
         print(f"Running Twitter script on device with IP: {device_ip}")
         twi.main(device)  # Assuming twi.main is the function for running the Twitter script
         time.sleep(5)  # Delay between scripts
+        open_vpn(device)
 
         print(f"Running TikTok script on device with IP: {device_ip}")
         tik.main(device)  # Assuming tik.main is the function for running the TikTok script
         time.sleep(5)  # Delay between scripts
+        open_vpn(device)
 
         print(f"Device with IP {device_ip} completed its tasks.")
         
