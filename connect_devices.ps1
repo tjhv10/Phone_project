44be5da6826dd9684dc76839d4910db3fbb19e22
tjhv10 <achiyabennatan@gmail.com>
2025-02-05 22:08:11 +0200

    Update comments in connect_devices.ps1 for clarity on script usage

--- connect_devices.ps1
+++ connect_devices.ps1
@@ -1,6 +1,7 @@
-for ($i=33; $i -le 40; $i++) {
+for ($i=10; $i -le 40; $i++) {
     $ip = "10.0.0." + ("{0:D2}" -f $i)
     Write-Host "Connecting to $ip..."
     & adb connect $ip
 }
-# powershell -ExecutionPolicy Bypass -File .\connect_devices.ps1    ############to run the script change numbers in the for loop to match your devices
+############to run the script change numbers in the for loop to match your devices ip address ends
+# powershell -ExecutionPolicy Bypass -File .\connect_devices.ps1
