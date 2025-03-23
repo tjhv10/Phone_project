f2ddaeda66f4608286d7869085fd9ba8dfdfa3b3
tjhv10 <achiyabennatan@gmail.com>
2025-03-23 18:26:33 +0200

    Update connect_devices_linux.sh to use dynamic range for IP connections and improve error handling

--- connect_devices_linux.sh
+++ connect_devices_linux.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-for i in $(seq 91 160); do
+buttom_str=$(python3 env.py buttom)
+top_str=$(python3 env.py top)
+if [ -z "$buttom_str" ] || [ -z "$top_str" ]; then
+    echo "Error: 'buttom' or 'top' is not set correctly."
+    exit 1
+fi
+for i in $(seq $buttom_str $top_str); do
     ip="10.0.0.$i"
     echo "Connecting to $ip..."
-    timeout 10s adb disconnect $ip || echo "Failed to connect to $ip, skipping..."
+    timeout 10s adb connect $ip || echo "Failed to connect to $ip, skipping..."
 done
-
 # Usage:
 # 1. Connect all devices to the same network
 # 2. Run the following commands one after another:
 #    chmod +x connect_devices_linux.sh
 #    ./connect_devices_linux.sh
 # 3. Enjoy!
-
-
-
-
-
